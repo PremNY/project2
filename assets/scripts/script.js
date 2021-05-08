70a605c1fe027905c2123d118e1b1a62270c19be
Prem Nankoo <pnankoo89@gmail.com>
2021-05-08 17:57:34 -0400

    Js Upload

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,181 +1,51 @@
-// var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-// var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-//   return new bootstrap.Popover(popoverTriggerEl)
-// })
+
+function validate() {
+  let firstName = document.getElementById('inputFirstName');
+  let lastName = document.getElementById('inputLastName');
+  let address = document.getElementById('inputAddress');
+  let city = document.getElementById('inputCity');
+
+  event.preventDefault();
+  if(firstName.value === '' || firstName.value.length === 0) {
+    alert('First Name must be filled');
+  } else if(lastName.value === '' || lastName.value.length ===0) {
+    alert('Last Name must be filled');
+  } else if(address.value === ''|| address.value.length === 0) {
+    alert('Please enter your address');
+  } else if (city.value === '' || city.value.length === 0) {
+    alert('Please enter your city');
+  }else {
+    ;
+  }
+
+}
 
 
-// var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
-//   container: 'body'
-// })
-
-// let zip = document.querySelector('input[name="inputZip"]');
-// let x =[11419, 11417];
-// let firstName = document.querySelector('input[name="inputFirstName"]');
-// document.getElementById("seeresults").style.display = "none";
-// seeresults = "discount"
-
-// function discount(){
-
-//   if (zip === 11419){
-//     console.log( firstName + 'You are eligible for 25% off your next bill, please provide us a valid driver\s license at the diner to redeem');
-//   } else if (zip === 11417){
-//     ( firstName + 'You are eligible for 5% off your next bill, please provide us a valid driver\s license at the diner to redeem');
-//   } else{
-//       ('Sorry you have to pay full price');
-//   }
-//   this.getElementById("seeresults").innerHTML = discount;
-//   document.getElementById("seeresults").innerHTML = discount;
-
-// }
-
-
-
-
-
-// let zip = document.querySelector('input[name="inputZip"]');
-// let x =[11419, 11417];
-// let firstName = document.querySelector('input[name="inputFirstName"]');
-// // document.getElementById("seeresults").style.display = "none";
-// seeresults = "discount"
-// let viewDetail = document.querySelector('viewDetail');
- 
-// function discount(){
-//   if (zip === 11419){
-//     console.log( firstName + 'You are eligible for 25% off your next bill, please provide us a valid driver\s license at the diner to redeem');
-//   } else if (zip === 11417){
-//     ( firstName + 'You are eligible for 5% off your next bill, please provide us a valid driver\s license at the diner to redeem');
-//   } else{
-//       ('Sorry you have to pay full price');
-//   }
-// }
-
-
-
-// let zip = document.querySelector('input[name="inputZip"]');
-// let firstName = document.querySelector('input[name="inputFirstName"]');
-
-
-
-
-
-// function discount(){
-//   if (zip === 11419){
-//     console.log( firstName + 'You are eligible for 25% off your next bill, please provide us a valid driver\s license at the diner to redeem');
-//   } else if (zip === 11417){
-//     console.log( firstName + 'You are eligible for 5% off your next bill, please provide us a valid driver\s license at the diner to redeem');
-//   } else{
-//     console.log('Sorry you have to pay full price');
-//   }
-
-
-
-// }
-
-// document.getElementById("seeresults").innerHTML = "Hello Dear Visitor!</br> We are happy that you've chosen our website to learn programming languages. We're sure you'll become one of the best programmers in your country. Good luck to you!";
-
-
-
-// let zip = document.querySelector('input[name="inputZip"]');
-// let firstName = document.querySelector('input[name="inputFirstName"]');
-// seeresults = document.getElementById("seeresults").style.display = "none";
-
-// function discount(){
-//   if (document.getElementById('inputZip').value = 11419){
-//     console.log( firstName + 'You are eligible for 25% off your next bill, please provide us a valid driver\s license at the diner to redeem');
-//   } else if (zip === 11417){
-//     console.log( firstName + 'You are eligible for 5% off your next bill, please provide us a valid driver\s license at the diner to redeem');
-//   } else{
-//     console.log('Sorry you have to pay full price');
-   
-//   }
+function discount() {
+  let zip = document.getElementById("inputZip").value;
+  let greeting;
+  let x = zip
   
-//   seeresults = document.getElementById("seeresults").style.display = "block";
-//   document.getElementById("seeresults").innerHTML = "discount";
-// }
+  if (x === "11419") {
+    greeting = "You are eligible for <strong>25%</strong> off your next bill, please provide us a valid driver\s license at the diner to redeem.";
+  } else if (x === "11417") {
+    greeting = "You are eligible for <strong>5%</strong> off your next bill, please provide us a valid driver\s license at the diner to redeem";
+  } else {
+    greeting = "Sorry you have to pay full price";
+  }
 
-
-
-// function myFunction() {
+  document.getElementById("seeresults").innerHTML = greeting;
   
-//   var greeting;
-//   var time = new Date().getHours();
-//   if (time < 10) {
-//     greeting = "Good morning";
-//   } else if (time < 20) {
-//     greeting = "Good day";
-//   } else {
-//     greeting = "Good evening";
-//   }
-//   document.getElementById("demo").innerHTML = greeting;
-// }
+}
 
 
-
-// function discount(){
-//   event.preventDefault();
-//   let zip = document.getElementById('inputZip');
-//   let greeting = zip;
-
-//   if(zip === 11419){
-//     greeting = "You are eligible for 25% off your next bill, please provide us a valid driver\s license at the diner to redeem";
-//   } else if(zip === 11417){
-//     greeting = "You are eligible for 5% off your next bill, please provide us a valid driver\s license at the diner to redeem";
-//   } else{
-//     greeting = "Sorry you have to pay full price";
-//   }
   
-//   document.getElementById("seeresults").innerHTML = greeting;
 
-// }
-
+  
 
 
 
 
 
 
-
-// function discount(){
-//   event.preventDefault();
-//   let zip = document.getElementById('inputZip');
-//   let greeting;
-
-//   if (zip = 11419){
-//       greeting = "You are eligible for 25% off your next bill, please provide us a valid driver\s license at the diner to redeem";
-//   } else if(zip = 11417){
-//       greeting = "You are eligible for 5% off your next bill, please provide us a valid driver\s license at the diner to redeem";
-//   } else {
-//       greeting = "Sorry you have to pay full price";
-//   }
-//     document.getElementById("seeresults").innerHTML = greeting;
-// }
-
-
-
-// function validateform(){  
-//   var name=document.myform.name.value;  
-//   var password=document.myform.password.value;  
-    
-//   if (name==null || name==""){  
-//     alert("Name can't be blank");  
-//     return false;  
-//   }else if(password.length<6){  
-//     alert("Password must be at least 6 characters long.");  
-//     return false;  
-//     }  
-//   }  
-
-
-// function validateform(){  
-//   var name=document.myform.name.value;  
-//   var password=document.myform.password.value;  
-    
-//   if (name==null || name==""){  
-//     alert("Name can't be blank");  
-//     return false;  
-//   }else if(password.length<6){  
-//     alert("Password must be at least 6 characters long.");  
-//     return false;  
-//     }  
-//   }  
 
