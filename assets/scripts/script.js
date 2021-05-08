0609181ec226e7b8e243d824770f5f909afc1360
Prem Nankoo <pnankoo89@gmail.com>
2021-05-08 11:41:46 -0400

    First commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,181 @@
+// var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+// var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+//   return new bootstrap.Popover(popoverTriggerEl)
+// })
+
+
+// var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+//   container: 'body'
+// })
+
+// let zip = document.querySelector('input[name="inputZip"]');
+// let x =[11419, 11417];
+// let firstName = document.querySelector('input[name="inputFirstName"]');
+// document.getElementById("seeresults").style.display = "none";
+// seeresults = "discount"
+
+// function discount(){
+
+//   if (zip === 11419){
+//     console.log( firstName + 'You are eligible for 25% off your next bill, please provide us a valid driver\s license at the diner to redeem');
+//   } else if (zip === 11417){
+//     ( firstName + 'You are eligible for 5% off your next bill, please provide us a valid driver\s license at the diner to redeem');
+//   } else{
+//       ('Sorry you have to pay full price');
+//   }
+//   this.getElementById("seeresults").innerHTML = discount;
+//   document.getElementById("seeresults").innerHTML = discount;
+
+// }
+
+
+
+
+
+// let zip = document.querySelector('input[name="inputZip"]');
+// let x =[11419, 11417];
+// let firstName = document.querySelector('input[name="inputFirstName"]');
+// // document.getElementById("seeresults").style.display = "none";
+// seeresults = "discount"
+// let viewDetail = document.querySelector('viewDetail');
+ 
+// function discount(){
+//   if (zip === 11419){
+//     console.log( firstName + 'You are eligible for 25% off your next bill, please provide us a valid driver\s license at the diner to redeem');
+//   } else if (zip === 11417){
+//     ( firstName + 'You are eligible for 5% off your next bill, please provide us a valid driver\s license at the diner to redeem');
+//   } else{
+//       ('Sorry you have to pay full price');
+//   }
+// }
+
+
+
+// let zip = document.querySelector('input[name="inputZip"]');
+// let firstName = document.querySelector('input[name="inputFirstName"]');
+
+
+
+
+
+// function discount(){
+//   if (zip === 11419){
+//     console.log( firstName + 'You are eligible for 25% off your next bill, please provide us a valid driver\s license at the diner to redeem');
+//   } else if (zip === 11417){
+//     console.log( firstName + 'You are eligible for 5% off your next bill, please provide us a valid driver\s license at the diner to redeem');
+//   } else{
+//     console.log('Sorry you have to pay full price');
+//   }
+
+
+
+// }
+
+// document.getElementById("seeresults").innerHTML = "Hello Dear Visitor!</br> We are happy that you've chosen our website to learn programming languages. We're sure you'll become one of the best programmers in your country. Good luck to you!";
+
+
+
+// let zip = document.querySelector('input[name="inputZip"]');
+// let firstName = document.querySelector('input[name="inputFirstName"]');
+// seeresults = document.getElementById("seeresults").style.display = "none";
+
+// function discount(){
+//   if (document.getElementById('inputZip').value = 11419){
+//     console.log( firstName + 'You are eligible for 25% off your next bill, please provide us a valid driver\s license at the diner to redeem');
+//   } else if (zip === 11417){
+//     console.log( firstName + 'You are eligible for 5% off your next bill, please provide us a valid driver\s license at the diner to redeem');
+//   } else{
+//     console.log('Sorry you have to pay full price');
+   
+//   }
+  
+//   seeresults = document.getElementById("seeresults").style.display = "block";
+//   document.getElementById("seeresults").innerHTML = "discount";
+// }
+
+
+
+// function myFunction() {
+  
+//   var greeting;
+//   var time = new Date().getHours();
+//   if (time < 10) {
+//     greeting = "Good morning";
+//   } else if (time < 20) {
+//     greeting = "Good day";
+//   } else {
+//     greeting = "Good evening";
+//   }
+//   document.getElementById("demo").innerHTML = greeting;
+// }
+
+
+
+// function discount(){
+//   event.preventDefault();
+//   let zip = document.getElementById('inputZip');
+//   let greeting = zip;
+
+//   if(zip === 11419){
+//     greeting = "You are eligible for 25% off your next bill, please provide us a valid driver\s license at the diner to redeem";
+//   } else if(zip === 11417){
+//     greeting = "You are eligible for 5% off your next bill, please provide us a valid driver\s license at the diner to redeem";
+//   } else{
+//     greeting = "Sorry you have to pay full price";
+//   }
+  
+//   document.getElementById("seeresults").innerHTML = greeting;
+
+// }
+
+
+
+
+
+
+
+
+// function discount(){
+//   event.preventDefault();
+//   let zip = document.getElementById('inputZip');
+//   let greeting;
+
+//   if (zip = 11419){
+//       greeting = "You are eligible for 25% off your next bill, please provide us a valid driver\s license at the diner to redeem";
+//   } else if(zip = 11417){
+//       greeting = "You are eligible for 5% off your next bill, please provide us a valid driver\s license at the diner to redeem";
+//   } else {
+//       greeting = "Sorry you have to pay full price";
+//   }
+//     document.getElementById("seeresults").innerHTML = greeting;
+// }
+
+
+
+// function validateform(){  
+//   var name=document.myform.name.value;  
+//   var password=document.myform.password.value;  
+    
+//   if (name==null || name==""){  
+//     alert("Name can't be blank");  
+//     return false;  
+//   }else if(password.length<6){  
+//     alert("Password must be at least 6 characters long.");  
+//     return false;  
+//     }  
+//   }  
+
+
+// function validateform(){  
+//   var name=document.myform.name.value;  
+//   var password=document.myform.password.value;  
+    
+//   if (name==null || name==""){  
+//     alert("Name can't be blank");  
+//     return false;  
+//   }else if(password.length<6){  
+//     alert("Password must be at least 6 characters long.");  
+//     return false;  
+//     }  
+//   }  
+
