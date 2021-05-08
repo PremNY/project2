1778de5c5fa39a6e231b35872f900cd01ebc96a4
Prem Nankoo <pnankoo89@gmail.com>
2021-05-08 18:41:32 -0400

    Final

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,7 @@ function validate() {
   let address = document.getElementById('inputAddress');
   let city = document.getElementById('inputCity');
 
+
   event.preventDefault();
   if(firstName.value === '' || firstName.value.length === 0) {
     alert('First Name must be filled');
@@ -15,23 +16,25 @@ function validate() {
   } else if (city.value === '' || city.value.length === 0) {
     alert('Please enter your city');
   }else {
-    ;
+    alert('Please fill all the fields'); ;
   }
 
 }
 
 
+
 function discount() {
   let zip = document.getElementById("inputZip").value;
+  let firstName = document.getElementById("inputFirstName").value;
   let greeting;
   let x = zip
-  
+
   if (x === "11419") {
-    greeting = "You are eligible for <strong>25%</strong> off your next bill, please provide us a valid driver\s license at the diner to redeem.";
+    greeting = firstName + ' ' + "you are eligible for <strong>25%</strong> off your next bill, please provide us a valid driver\s license at the diner to redeem.";
   } else if (x === "11417") {
-    greeting = "You are eligible for <strong>5%</strong> off your next bill, please provide us a valid driver\s license at the diner to redeem";
+    greeting = firstName + ' ' + "you are eligible for <strong>5%</strong> off your next bill, please provide us a valid driver\s license at the diner to redeem.";
   } else {
-    greeting = "Sorry you have to pay full price";
+    greeting = firstName + ' ' + "sorry you have to pay full price.";
   }
 
   document.getElementById("seeresults").innerHTML = greeting;
